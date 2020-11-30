a6d07bb8ba7a95ed644b18bbf7e14ac11a5b6eb9
Jim Porter <itsjimporter@gmail.com>
2020-11-29 18:36:49 -0800

    Add CI for Python 3.9

--- setup.py
+++ setup.py
@@ -252,6 +252,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     cmdclass=custom_cmds,
